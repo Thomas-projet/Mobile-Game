1f7aa01b2d6c3549ad85037458ff3487ec05d7e5
Thomas-projet <thomas.callegari@efrei.net>
2021-12-21 15:06:54 +0100

    fireball target the enemy

--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    public float speed = 20f;
+    private float speed = 5f;
     public Rigidbody2D rb;
+
+    [SerializeField]
+    private GameManager GM;
+    private GameObject target;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = transform.right * speed; 
+        GM = FindObjectOfType<GameManager>();
+        target = GM.Enemy;
+
+    }
+
+
+    private void Update()
+    {
+        Vector2 dir = target.transform.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
